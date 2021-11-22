92ae65f1f1e8197b1755aae96ef25ef533ca2658
Rahul Mittal <mittalrahul074@gmail.com>
2021-11-22 13:52:44 +0530

    xamp

--- authentication.php
+++ authentication.php
@@ -14,8 +14,9 @@
         $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
         $count = mysqli_num_rows($result);  
           
-        if($count == 1){  
-            echo "<h1><center> Login successful </center></h1>";  
+        if($count == 1){
+            header("Location: http://localhost/project/afterhome.html");
+            exit();  
         }  
         else{  
             echo "<h1> Login failed. Invalid username or password.</h1>";  
